b9259c4eb4879a8476d640d4cd1014e4b03fa248
Paul8710 <paulletellierpro@gmail.com>
2024-09-22 11:55:00 +0200

    ajout de commentaires

--- script/script.js
+++ script/script.js
@@ -1,11 +1,21 @@
 let minute = 0;
 let seconde = 15;
+// la variable id permet de stocker l'id de l'intervalle afin de pouvoir l'arreter
 let id;
+// permet de savoir si le prochain est un temps de pause ou non
 let pause = true;
+// lie le bouton HTML au javascript
+let bouton = document.getElementById('monBouton');
+// lance la fonction lancerDecompte quand le bouton est cliqué
+bouton.addEventListener('click', lancerDecompte);
 
+//Affichage Initiale
 document.getElementById("intitule").textContent = "Temps de travail =";
 document.getElementById("affichage").textContent = minute + ":" + seconde;
 
+
+//Permet de décompter le temps
+//Prend en charge les temps de pause et travail
 function decompte() {
     
     if(seconde == 0 && minute !=0){
@@ -34,11 +44,7 @@ function decompte() {
     
 }
 
-
-
-let bouton = document.getElementById('monBouton');
-
-
+//Permet de rénitialiser le décompte quand on clique sur le bouton
 function arretDecompte(){
     clearInterval(id);
     minute = 25;
@@ -49,7 +55,7 @@ function arretDecompte(){
     bouton.addEventListener('click', lancerDecompte);
 }
 
-
+//Permet de lancer le décompte quand on clique sur le bouton
 function lancerDecompte(){
     id = setInterval(decompte,1000);
     document.getElementById("monBouton").textContent = "⏹︎";
@@ -58,4 +64,4 @@ function lancerDecompte(){
     
 }
 
-bouton.addEventListener('click', lancerDecompte);
+
